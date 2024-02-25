3ff0d677fdd8693c5adbbfb994a176a41d5a4871
SujanSapkota07 <sujeansapkota47@gmail.com>
2024-02-25 19:12:06 +0545

    view button on dashboard working now

--- Nepa_application/tempCodeRunnerFile.py
+++ Nepa_application/tempCodeRunnerFile.py
@@ -1,3 +1,14 @@
-user = User.objects.get(username='example_user')  # Replace 'example_user' with the username of the user you want to get comments for
-total_comments = get_total_comments_for_user(user)
-print(total_comments)  
+
+def containsDuplicate(nums):
+    unique_elements = set()
+    # print(unique_elements)
+    for num in nums:
+        print(unique_elements)
+        if num in unique_elements:
+            return True
+        else:
+            unique_elements.add(num)
+    return False
+
+nums = [1,2,3,4,5,6,7,8,1,9,10,11,12,13,14,15,1]
+print(containsDuplicate(nums))
